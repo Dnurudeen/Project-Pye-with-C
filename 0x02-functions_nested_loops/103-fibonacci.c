97a92ccb75a076c3930b7095de4e2b2764ca97d4
root <danijunurdeen@gmail.com>
2021-06-23 16:49:50 -0700

    commit advance

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - prints sum of fibo numbers under 4,000,000
+ * main - print sum of even Fionacci sequence up to 4,000,000
  * Return: 0
  */
 int main(void)
 {
-long int f1 = 1, f2 = 2, sum = f2, tmp;
-while (f2 < 4000000)
+int sum_of_evens = 0;
+int a;
+int b;
+int sum = 1;
+a = 1;
+b = 1;
+while (b < 4000000)
 {
-tmp = f1 + f2;
-f1 = f2;
-f2 = tmp;
-sum += (f2 % 2 == 0) ? f2 : 0;
+sum = a + b;
+a = b;
+b = sum;
+if ((sum <= 4000000) && (sum % 2 == 0))
+sum_of_evens += sum;
 }
-printf("%li\n", sum);
+printf("%d\n", sum_of_evens);
 return (0);
 }
