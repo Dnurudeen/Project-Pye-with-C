045a54080f2a5cbcbb8a6efa205ceaafd7f67aee
root <danijunurdeen@gmail.com>
2021-08-09 07:23:37 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,37 @@
 #include "holberton.h"
-
 /**
- *read_textfile - reads a text file and
- *prints it to the POSIX standard output
- *@filename: file pointer
- *@letters: number of letters it should read and print
- *Return: actual number of letters it could read and print
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
   int fd;
-  ssize_t fread, fwrite;
-  char *buffer;
-
-  if (filename == NULL)
+  int i, y;
+  char *buf;
+  if (!filename)
     return (0);
-
-  /*open file*/
   fd = open(filename, O_RDONLY);
-  if (fd == -1)
+  if (fd < 0)
     return (0);
-
-  /*malloc buffer*/
-  buffer = (char *)malloc(sizeof(char) * letters);
-  if (buffer == NULL)
+  buf = malloc(sizeof(char) * letters);
+  if (!buf)
     return (0);
-
-  /*read file*/
-  fread = read(fd, buffer, letters);
-  if (fread == -1)
+  i = read(fd, buf, letters);
+  if (i < 0)
     {
-      free(buffer);
-      close(fread);
+      free(buf);
       return (0);
     }
-
-  /*write file*/
-  fwrite = write(STDOUT_FILENO, buffer, fread);
-  if (fwrite == -1)
-    {
-      free(buffer);
-      close(fd);
-      return (0);
-    }
-
+  buf[i] = '\0';
   close(fd);
-  return (fread);
+  y = write(STDOUT_FILENO, buf, i);
+  if (y < 0)
+    {
+      free(buf);
+      return (0);
+    }
+  free(buf);
+  return (y);
 }
