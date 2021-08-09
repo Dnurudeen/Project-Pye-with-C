1ddfeb7af5409a74d2c60486eda7bf2dddc034ab
root <danijunurdeen@gmail.com>
2021-08-09 06:58:58 -0700

    commit corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,55 @@
 #include "holberton.h"
 
 /**
- * create_file - creates a file
- * @filename: pointer to filename
- * @text_content: string that is appended to file
- * Return: 1 on success, -1 on failure
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
+ * create_file - creates file with permissions rw------- and writes content in
+ * if file already exists, don't change permissions and just truncate it
+ * @filename: name to give to new file
+ * @text_content: writes this content into file
+ * Return: 1 on success, -1 on error
  */
 int create_file(const char *filename, char *text_content)
 {
-	register int fd, w, len = 0;
+	int fd;
+	int n_wrote;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	/* create with permissions if file doesn't exist, else truncate */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
+
+	/* if nothing to write, just return newly created file */
+	if (!text_content)
 	{
-		while (text_content[len])
-			len++;
-		w = write(fd, text_content, len);
-		if (w == -1)
-			return (-1);
+		close(fd);
+		return (1);
 	}
+
+	/* write */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
 	close(fd);
 	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,54 @@
 #include "holberton.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: pointer to filename
- * @text_content: string to be appended
- * Return: 1 on success, -1 on failure
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
+ * append_text_to_file - append text and only if file exists
+ * @filename: file
+ * @text_content: appends this content into file
+ * Return: 1 on success, -1 on error
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	register int fd, w, len = 0;
+	int fd;
+	int n_wrote;
 
 	if (!filename)
 		return (-1);
+
+	/* open file if it exists */
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
+
+	/* if nothing to write, still successful */
+	if (!text_content)
 	{
-		while (text_content[len])
-			len++;
-		w = write(fd, text_content, len);
-		if (w == -1)
-			return (-1);
+		close(fd);
+		return (1);
 	}
+
+	/* write */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
 	close(fd);
 	return (1);
 }
